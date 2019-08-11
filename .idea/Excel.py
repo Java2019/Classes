3420d0c859e4d02f7e2f477880990e135f7950ea
java2019 <java_2019@mail.ru>
2019-08-11 17:48:21 +0300

    *

--- .idea/Excel.py
+++ .idea/Excel.py
@@ -3,15 +3,16 @@ import xlrd
 def get_data():
 
     excel = xlrd.open_workbook('~/Python/lec3/Excel/Взносы.xls')
-    sheet = excel.sheet_by_index(1)
+    sheet = excel.sheet_by_index(0)
 
     row_number = sheet.nrows
 
     table = []
 
     if not row_number == 0:
-        for i in range(0, row_number):
-            table.append(str(sheet.row(i)[0]))
+        table = [str(sheet.row(i)[0]) for i in range(0,row_number)]
+        #for i in range(0, row_number):
+        #    table.append(str(sheet.row(i)[0]))
     else:
         print('Указанный файл не заполнен')
 

b45b4d790b92836db5737b408a0fea295d61c974
java2019 <java_2019@mail.ru>
2019-08-11 21:44:35 +0300

    *

--- Excel.py
+++ Excel.py
@@ -4,3 +4,10 @@ import pprint
 Excel = GetDataFromExcel('~/Python/lec3/Excel/Взносы.xls')
 
 pprint.pprint(Excel.get_data())
+
+s = int(input('Enter the number: '))
+
+while s < 0:
+    s = int(input('Enter the number: '))
+
+print(s + 3)
